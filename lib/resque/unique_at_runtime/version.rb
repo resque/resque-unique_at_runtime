b9a4155d5cac0705e8e120f8ffe85ab550c629a8
Peter H. Boling <peter.boling@gmail.com>
2026-08-01 15:32:13 -0600

    🔖 Bump gem version

--- lib/resque/unique_at_runtime/version.rb
+++ lib/resque/unique_at_runtime/version.rb
@@ -5,7 +5,7 @@ module Resque
     # Version namespace for this gem.
     module Version
       # Current gem version.
-      VERSION = "4.0.1"
+      VERSION = "4.0.2"
     end
     # Current gem version exposed at the traditional constant location.
     VERSION = Version::VERSION # Traditional Constant Location
