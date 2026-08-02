3d4de7b9060afb8ec8c721e6c47e3710d937bb9d
Peter H. Boling <peter.boling@gmail.com>
2026-08-01 20:45:13 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- lib/resque/unique_at_runtime.rb
+++ lib/resque/unique_at_runtime.rb
@@ -1,13 +1,8 @@
 # frozen_string_literal: true
 
-require "version_gem"
 require_relative "unique_at_runtime/version"
 
 module Resque
   module UniqueAtRuntime
   end
-end
-
-Resque::UniqueAtRuntime::Version.class_eval do
-  extend VersionGem::Basic
 end
